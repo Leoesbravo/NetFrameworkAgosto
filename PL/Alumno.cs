19ea51a91f2395450fd6b3373e34d4433718a68a
AngelMendoza1 <89880077+AngelMendoza1@users.noreply.github.com>
2022-08-17 14:15:34 -0500

    Add project files.

---
+++ PL/Alumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Alumno
+    {
+        public static void Add()
+        {
+            ML.Alumno alumno = new ML.Alumno();//instancia de una clase || Crear un objeto para acceder a atributos y metodos
+
+            Console.WriteLine("Inserte el nombre del Alumno");
+            alumno.Nombre = Console.ReadLine();
+            //string nombre = Console.ReadLine();
+
+            Console.WriteLine("Inserte el apellido paterno del Alumno");
+            alumno.ApellidoPaterno = Console.ReadLine();
+
+            Console.WriteLine("Inserte el apellido materno del Alumno");
+            alumno.ApellidoMaterno = Console.ReadLine();
+
+            
+
+        }
+    }
+}
