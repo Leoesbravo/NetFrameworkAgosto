3b7df35ef7c8d9b891db960f681cad01e4376ef1
AngelMendoza1 <89880077+AngelMendoza1@users.noreply.github.com>
2022-08-29 17:08:33 -0500

    MVC

--- ML/Alumno.cs
+++ ML/Alumno.cs
@@ -14,6 +14,7 @@ namespace ML
         public string ApellidoMaterno { get; set; }
         public string FechaNacimiento { get; set; }
         public string Sexo { get; set; }
+        public List<object> Alumnos { get; set; }
         public  ML.Semestre Semestre { get; set; } // propiedad de navegación
     }
 }
---
+++ PL_MVC/Controllers/AlumnoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL_MVC.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        [HttpGet]//Action Verb
+        public ActionResult GetAll() //Action Method
+        {
+            ML.Result result = BL.Alumno.GetAllLINQ();
+            ML.Alumno alumno = new ML.Alumno();
+            if (result.Correct)
+            {    //lista vacia     //lista de alumnos
+                alumno.Alumnos = result.Objects; // estoy pasando de result.objects a alumno.Alumnos
+
+                return View(alumno);
+            }
+            else
+            {
+                return View(alumno);
+            }
+
+        }
+    }
+}
